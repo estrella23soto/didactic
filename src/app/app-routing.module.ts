87f557442b9c345d4a7cce1eaaa0add431a7918c
estrella <estrellajuarez8soto@gmail.com>
2022-06-10 10:52:22 -0500

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { InstructorComponent } from './componenents/instructor/instructor.component';
+import { Instructor3Component } from './componenents/instructor3/instructor3.component';
+import { Instructor2Component } from './componenents/instructor2/instructor2.component';
+import { Instructor1Component } from './componenents/instructor1/instructor1.component';
+import { AdministradorComponent } from './componenents/administrador/administrador.component';
+import { AlumnosComponent } from './componenents/alumnos/alumnos.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'alumnos',component:AlumnosComponent},
+  {path:'administrador',component:AdministradorComponent},
+  {path:'instructor1',component:Instructor1Component},
+  {path:'instructor2',component:Instructor2Component},
+  {path:'instructor3',component:Instructor3Component},
+  {path:'instructor',component:InstructorComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AlumnosComponent } from './componenents/alumnos/alumnos.component';
+import { AdministradorComponent } from './componenents/administrador/administrador.component';
+import { Instructor1Component } from './componenents/instructor1/instructor1.component';
+import { Instructor2Component } from './componenents/instructor2/instructor2.component';
+import { LogeoComponent } from './components/logeo/logeo.component';
+import { Instructor3Component } from './componenents/instructor3/instructor3.component';
+import { InstructorComponent } from './componenents/instructor/instructor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlumnosComponent,
+    AdministradorComponent,
+    Instructor1Component,
+    Instructor2Component,
+    LogeoComponent,
+    Instructor3Component,
+    InstructorComponent
   ],
   imports: [
     BrowserModule,
